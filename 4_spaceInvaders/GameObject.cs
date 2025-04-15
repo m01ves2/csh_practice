688f5f3b631cae9d2533ff1a369c9377ff9493d2
Eugeny Vlasov <eugeny.vlasov@gmail.com>
2025-04-15 12:49:53 +0300

    initial commit of game 'Space Invaders'

---
+++ 4_spaceInvaders/GameObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_spaceInvaders
+{
+    internal abstract class GameObject
+    {
+        public Point location {  get; set; }
+    }
+}
